e3fa210bdc965066e759c4b3e367dc999e3b86b9
Abdul Wasey BH <abdul.wasey@baytalhulool.com>
2026-01-13 16:38:53 +0500

    Created backups table.

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Currency;
 use Illuminate\Http\Request;
 
 class CurrencyController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $currencies = Currency::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'Currencies fetched successfully',
+            'data' => $currencies,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\AccountHeadController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\BackupController;
 use App\Http\Controllers\BimonthlyReceiptController;
@@ -36,6 +37,8 @@ Route::prefix('/auth')->group(function () {
     // Route::get('/user', [UserController::class, 'show'])
     //     ->middleware('auth:sanctum');
 });
+
+Route::get('/account-heads', [AccountHeadController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/', [HomeController::class, 'index']);
